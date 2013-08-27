98f9482d3c59c4fdd37b50de1f166223b56843b6
daroprito <daroprito_94@hotmail.com>
2013-08-27 09:39:29 -0500

    Workspace

---
+++ workspace/programación/src/programación/operación.java
@@ -0,0 +1,60 @@
+package programación;
+
+public class operación {
+
+	int x,y; 
+	
+	public static void main (String m []){
+		operación  t=new operación();
+		t.x=7;
+		t.y=8;
+		t.suma();
+		t.resta();
+		t.multiplicacion();
+		t.division();
+	
+	}
+	public void suma () {
+		int r; 
+		r= x+y;
+		System.out.println("la suma es"+r);
+	}
+
+
+
+
+public void resta () {
+	
+
+ 
+	int r; 
+	r= x-y;
+	System.out.println("la resta es"+r);
+}
+
+
+
+public void multiplicacion() {
+	
+
+	 
+	int r; 
+	r= x*y;
+	System.out.println("la multiplicacion es"+r);
+}
+
+public void division () {
+	
+
+	 
+	int r; 
+	r= x/y;
+	System.out.println("la division es"+r);
+}
+
+}
+
+
+
+
+
